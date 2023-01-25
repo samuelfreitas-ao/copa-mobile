056e3efa2bc8c87b473d3fcf0b2cfd63976f495d
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-01-25 20:25:54 +0100

    feat: screens

---
+++ src/Screens/Pool.tsx
@@ -0,0 +1,19 @@
+import { Icon, VStack } from "native-base";
+import { Octicons } from '@expo/vector-icons'
+
+import { Header } from "../components/Header";
+import { Button } from "../components/Button";
+
+export function Find () {
+  return (
+    <VStack flex={1} bgColor="gray.900">
+      <Header title="Buscar por código" />
+      <VStack mt={8} mx={5} alignItems="center">
+        <Button
+          title="Buscar boão por código"
+          leftIcon={<Icon as={Octicons} />}
+        />
+      </VStack>
+    </VStack>
+  )
+}
--- src/Screens/Signin.tsx
+++ src/Screens/Signin.tsx
@@ -1,8 +1,33 @@
-import { Center, Text } from 'native-base'
+import { Center, Icon, Text } from 'native-base'
+import { Fontisto } from '@expo/vector-icons'
+
+import Logo from '../assets/logo.svg'
+import { Button } from '../components/Button'
+import { useAuth } from '../hooks/useAuth'
+
 export function Signin () {
+  const { signIn, user } = useAuth()
+
   return (
     <Center flex={1} bgColor="gray.900" >
-      <Text color="white" fontSize={24}>Hello world</Text>
+      <Logo width={212} height={40} />.
+
+      <Button
+        title='ENTRAR COM O GOOGLE'
+        leftIcon={<Icon as={Fontisto} name='google' />}
+        type='SECONDARY'
+        mt={12}
+        onPress={signIn}
+      />
+
+      <Text
+        color="white"
+        textAlign="center"
+        mt={4}
+      >
+        Não utilizamos nenhuma informação além {'\n'}
+        do seu email para criação da sua conta.
+      </Text>
     </Center>
   )
 }
