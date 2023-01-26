9fc5686950f80a821609d069a816231d79163c26
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-01-26 14:12:34 +0100

    feat: screen navigation

--- src/Screens/Poll.tsx
+++ src/Screens/Poll.tsx
@@ -1,17 +1,21 @@
 import { Icon, VStack } from "native-base";
 import { Octicons } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
 
 import { Header } from "../components/Header";
 import { Button } from "../components/Button";
 
 export function Poll () {
+  const { navigate } = useNavigation()
+
   return (
     <VStack flex={1} bgColor="gray.900">
       <Header title="Buscar por código" />
       <VStack mt={8} mx={5} alignItems="center">
         <Button
-          title="Buscar boão por código"
-          leftIcon={<Icon as={Octicons} />}
+          title="BUSCAR BOLÃO POR CÓDIGO"
+          leftIcon={<Icon as={Octicons} name="search" color="black" size="md" />}
+          onPress={() => navigate('find')}
         />
       </VStack>
     </VStack>
--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,14 +1,59 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { PlusCircle, SoccerBall } from 'phosphor-react-native'
+import { useTheme } from 'native-base'
+
 import { New } from '../Screens/New'
 import { Poll } from '../Screens/Poll'
+import { Platform } from 'react-native'
+import { Find } from '../Screens/Find'
 
 const { Navigator, Screen } = createBottomTabNavigator()
 
 export function AppRoutes () {
+  const { colors, sizes } = useTheme()
+  const size = sizes[6]
   return (
-    <Navigator>
-      <Screen name='new' component={New} />
-      <Screen name='polls' component={Poll} />
+    <Navigator screenOptions={{
+      headerShown: false,
+      tabBarLabelPosition: 'beside-icon',
+      tabBarActiveTintColor: colors.yellow[500],
+      tabBarInactiveTintColor: colors.gray[300],
+      tabBarStyle: {
+        position: 'absolute',
+        height: sizes[22],
+        borderTopWidth: 0,
+        backgroundColor: colors.gray[800]
+      },
+      tabBarItemStyle: {
+        position: 'relative',
+        top: Platform.OS === 'android' ? -10 : 0
+      }
+    }}>
+      <Screen
+        name='new'
+        component={New}
+        options={{
+          tabBarIcon: ({ color }) => <PlusCircle color={color} size={size} />,
+          tabBarLabel: 'Novo bolão'
+
+        }} />
+
+      <Screen
+        name='polls'
+        component={Poll}
+        options={{
+          tabBarIcon: ({ color }) => <SoccerBall color={color} size={size} />,
+          tabBarLabel: 'Meus bolões'
+
+        }} />
+
+      <Screen
+        name='find'
+        component={Find}
+        options={{
+          tabBarButton: () => null
+        }}
+      />
     </Navigator>
   )
 }
